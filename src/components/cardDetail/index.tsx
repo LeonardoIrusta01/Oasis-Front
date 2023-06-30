c28c1ea57e89a34ee16343580822e14204d2e834
Luciano Carducci <carduccilucianof@gmail.com>
2023-06-30 15:58:51 -0300

    Card detail fixed

--- src/components/cardDetail/index.tsx
+++ src/components/cardDetail/index.tsx
@@ -45,22 +45,23 @@ const CardDetailComponent: React.FC<ICardDetail> = ({ id }) => {
 
 	return (
 		<>
-			<div className='w-full flex justify-center flex-col bg-oasisGradient-antiFlashWhite h-screen'>
-				<div className='w-full bg-oasisGradient-white'>
+			<Navbar />
+			<div className='w-full flex justify-center flex-col bg-oasisGradient-antiFlashWhite h-screen md:flex-row lg:flex-row'>
+				<div className='w-full bg-oasisGradient-white md:w-14 lg:w-14'>
 					<Link href={'/'} className='flex w-14'>
 						<Image
 							src={ArrowBackButton}
 							alt='ArrowBackButton'></Image>
 					</Link>
 				</div>
-				<div className='w-full flex items-center p-8 flex-col bg-oasisGradient-white'>
+				<div className='w-full flex items-center justify-center p-8 flex-col bg-oasisGradient-white md:flex-row md:w-1/2 lg:w-1/2'>
 					<Image
 						src={defaultIcon}
 						alt='default_Food_Icon'
 						className='w-52 h-52'
 					/>
 				</div>
-				<div className='w-full h-full bg-oasisGradient-antiFlashWhite p-8 space-y-6'>
+				<div className='w-full h-full bg-oasisGradient-antiFlashWhite p-8 space-y-6 md:items-center md:w-1/2 lg:w-1/2'>
 					<h1 className='text-3xl font-bold'>
 						{data?.payload?.name}
 					</h1>
@@ -71,7 +72,7 @@ const CardDetailComponent: React.FC<ICardDetail> = ({ id }) => {
 				</div>
 				<div
 					id='modal-container'
-					className='relative bottom-10 mx-5 h-48 md:h-64 align-bottom bg-oasisGradient-cambridgeBlue text-left overflow-hidden shadow-xl sm:my-8 sm:align-middle sm:max-w-lg w-screen/90'>
+					className='absolute bottom-5 mx-5 bg-oasisGradient-cambridgeBlue text-left overflow-hidden shadow-xl sm:my-8 align-middle max-w-md w-screen/90 lg:right-16 md:max-w-lg'>
 					<div
 						id='modal-wrapper'
 						className='bg-oasisGradient-cambridgeBlue p-4 sm:p-6 sm:pb-4'>
@@ -83,7 +84,7 @@ const CardDetailComponent: React.FC<ICardDetail> = ({ id }) => {
 								className='mt-2 flex flex-row w-full justify-evenly'>
 								<button onClick={decrementCount}>
 									<Image
-									className='w-10'
+										className='w-10'
 										src={MinusIcon}
 										alt='MinusIcon'></Image>
 								</button>
@@ -92,7 +93,7 @@ const CardDetailComponent: React.FC<ICardDetail> = ({ id }) => {
 										placeholder={`${count || 0}`}
 										className='text-lg text-gray-900 w-24 text-center rounded-md'
 									/>
-									<p className='pt-2 text-center'>
+									<p className='pt-2 flex justify-center'>
 										$
 										{data?.payload?.price
 											? data.payload.price * count
@@ -101,7 +102,7 @@ const CardDetailComponent: React.FC<ICardDetail> = ({ id }) => {
 								</div>
 								<button onClick={incrementCount}>
 									<Image
-									className='w-10 h-10'
+										className='w-10 h-10'
 										src={PlusIcon}
 										alt='PlusIcon'></Image>
 								</button>
