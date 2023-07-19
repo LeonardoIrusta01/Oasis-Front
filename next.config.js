1acf29c79dfc6eed92f8df22c95156f0c4c66d4d
Luciano Carducci <carduccilucianof@gmail.com>
2023-07-19 19:46:40 -0300

    User´s post to db and image upload component

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        formats: ['image/avif', 'image/webp'],
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'lh3.googleusercontent.com',
+          },
+        ],
+      },
+}
 
 module.exports = nextConfig
--- src/redux/services/user/usersApi.ts
+++ src/redux/services/user/usersApi.ts
@@ -1,5 +1,5 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { IApiUsersResponse, IpostResponse } from "./interface"
+import { IApiUsersResponse, IpostResponse, Iuser, IpostUserResponse, IApiUserResponse } from './interface';
 
 export const usersApi = createApi({
 	reducerPath: 'usersApi',
@@ -10,11 +10,26 @@ export const usersApi = createApi({
 		getUsers: builder.query<IApiUsersResponse, null>({
 			query: () => '/api/user',
 		}),
-		update: builder.mutation<IpostResponse, {id: string; post: FormData}>({
+		updateUsers: builder.mutation<
+			IpostResponse,
+			{ id: number; post: Iuser }
+		>({
 			query: ({ id, post }) => ({
 				url: `/api/user/${id}`,
-				method: 'PATCH',
+				method: 'PUT',
 				body: post,
+				headers: { 'Content-type': 'application/json; charset=UTF-8' },
+			}),
+		}),
+		getUserbyEmail: builder.query<IApiUserResponse, string | undefined>({
+			query: (email) => `/api/user?email=${email}`,
+		}),
+		postUser: builder.mutation<IpostUserResponse, Iuser>({
+			query: (post) => ({
+					url: '/api/user',
+					method: 'POST',
+					body: post,
+					headers: { 'Content-type': 'application/json; charset=UTF-8' },
 			}),
 		}),
 	}),
@@ -22,5 +37,7 @@ export const usersApi = createApi({
 
 export const {
 	useGetUsersQuery,
-	useUpdateMutation,
+	usePostUserMutation,
+	useUpdateUsersMutation,
+	useGetUserbyEmailQuery,
 } = usersApi;
